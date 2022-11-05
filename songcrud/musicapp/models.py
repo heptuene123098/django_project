bf6b1531e41fd5e14e6932211846c0315e088409
Otuene <heptuene@gmail.com>
2022-11-05 08:37:36 +0100

    update

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -3,26 +3,29 @@ from django.db import models
 # Create your models here.
 
 class Artiste(models.Model):  
-  first_name = models.CharField(max_length=20)  
-   last_name= models.CharField(max_length=100)  
-    age = models.()  
-    class Meta:  
+    first_name = models.CharField(max_length=20)  
+last_name= models.CharField(max_length=100)  
+age = models.IntegerField(_(""))()  
+    
+class Meta:  
         db_table = "artiste"  
-            def __str__(self):
-            return self.(first_name, last_name, age)
+        def __str__(self):
+            return (self.first_name, self.last_name, self.age)
         
 class Song(models.Model):  
   title = models.CharField(max_length=200)  
-   date_released= models.CharField(max_length=100)  
-    likes = models.EmailField()  
-    artiste_id = models.EmailField()  
-     class Meta:  
+date_released= models.CharField(max_length=100)  
+likes = models.ForeignKey(on_delete=models.CASCADE)  
+artiste_id = models.CharField(max_length=20)  
+class Meta:  
         db_table = "song"  
-    def __str__(self):
+def __str__(self):
         return self.title
     
 class Lyric(models.Model):  
- content = models.CharField(max_length=20)  
-   song_id= models.CharField(max_length=100)  
-    class Meta:  
+ content = models.CharField(max_length=200)  
+song_id= models.CharField(max_length=20)  
+class Meta:  
         db_table = "lyric"  
+def __str__(self):
+        return self.content
