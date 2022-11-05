e86ef2412c42d6172e261f5f60a4cce1b22ff85d
Otuene <heptuene@gmail.com>
2022-11-05 08:24:20 +0100

    update

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -5,11 +5,24 @@ from django.db import models
 class Artiste(models.Model):  
   first_name = models.CharField(max_length=20)  
    last_name= models.CharField(max_length=100)  
-    age = models.EmailField()  
-   
+    age = models.()  
     class Meta:  
-        db_table = "employee"  
-Model: , Song, Lyric
-Attributes for “Artiste” : , , 
-Attributes for “Song” : title, date released, likes, artiste_id
-Attributes for “Lyric”: content, song_id
+        db_table = "artiste"  
+            def __str__(self):
+            return self.(first_name, last_name, age)
+        
+class Song(models.Model):  
+  title = models.CharField(max_length=200)  
+   date_released= models.CharField(max_length=100)  
+    likes = models.EmailField()  
+    artiste_id = models.EmailField()  
+     class Meta:  
+        db_table = "song"  
+    def __str__(self):
+        return self.title
+    
+class Lyric(models.Model):  
+ content = models.CharField(max_length=20)  
+   song_id= models.CharField(max_length=100)  
+    class Meta:  
+        db_table = "lyric"  
